9ac0d312d82bd4e650d6a782404dfbff7d32bca6
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2023-04-14 21:11:13 -0300

    Add files via upload

---
+++ q11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, b;
+    puts("Digite a faixa que você deseja: ");
+    scanf("%d %d", &a, &b);
+
+    if (a < b){
+        for (int i = a; i < b; i++){
+
+        if (i % 4 == 0){
+            printf("O quadrado de %d é %d\n",i, i*i);
+        }
+    }
+
+    }
+
+    if (a > b){
+        for (int i = b; i < a; i++){
+
+        if (i % 4 == 0){
+            printf("O quadrado de %d é %d\n",i, i*i);
+        }
+    }
+
+    }
+
+
+
+    return 0;
+}
